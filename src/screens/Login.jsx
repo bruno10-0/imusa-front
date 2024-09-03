68d521dc6faec0ffd129e1b1105fcd7b232a74d1
bruno10-0 <brunobrodon75@gmail.com>
2024-09-03 08:26:47 -0300

    style: :lipstick: Mjeoras a la UI del Login

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -5,7 +5,6 @@ import { useNavigation } from "@react-navigation/native";
 import { Formik } from "formik";
 import LoginValidation from "../schemas/LoginValidation";
 import Animated, { FadeInDown, FadeInUp } from "react-native-reanimated";
-
 export default function Login() {
   const navigation = useNavigation();
   // Función para manejar el envío del formulario
@@ -27,112 +26,112 @@ export default function Login() {
   };
 
   return (
-    <View className="h-full w-full bg-black/100">
-      <StatusBar style="light" />
-
+    <View className="h-full w-full justify-center items-center">
       <Animated.Image
         entering={FadeInUp.delay(200).duration(1000)}
-        className="h-full w-full absolute opacity-60 object-fill"
+        className="h-full w-full absolute"
         source={{
-          uri: "https://th.bing.com/th/id/R.118a9a639e0f8370e2ed76cdd58e4133?rik=aQC%2bpLg3XF8Acg&pid=ImgRaw&r=0",
+          uri: "https://i.pinimg.com/736x/ce/9d/89/ce9d892863cdef05531c21645f80551f.jpg",
         }}
       />
+      <View className="h-full w-full flex justify-center items-center px-4">
+        <View className="h-auto w-full flex justify-center bg-black/5 p-2 py-10  backdrop-blur-lg rounded-2xl">
+          {/* Título */}
+          <View className="flex items-center">
+            <Animated.Text
+              entering={FadeInUp.delay(200).springify()}
+              className="text-primary font-bold text-7xl uppercase"
+            >
+              Data Pet
+            </Animated.Text>
+            <Animated.Text
+              entering={FadeInUp.delay(200).springify()}
+              className="text-white font-bold text-xl mb-5"
+            >
+              El mejor amigo de tu amigo
+            </Animated.Text>
+          </View>
 
-      {/* título y formulario */}
-      <View className="h-full w-full flex justify-around pt-40 pb-10">
-        {/* Título */}
-        <View className="flex items-center">
-          <Animated.Text
-            entering={FadeInUp.delay(200).springify()}
-            className="text-white font-bold text-5xl"
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={LoginValidation}
+            onSubmit={handleSubmit}
           >
-            YPF AGRO
-          </Animated.Text>
-          <Animated.Text
-            entering={FadeInUp.delay(200).springify()}
-            className="text-yellow-500 font-bold text-5xl"
-          >
-            TRACKING
-          </Animated.Text>
-        </View>
-
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={LoginValidation}
-          onSubmit={handleSubmit}
-        >
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit: formikSubmit,
-            values,
-            errors,
-            touched,
-          }) => (
-            <View className="flex items-center mx-4 space-y-4">
-              <Animated.View
-                entering={FadeInDown.delay(200).springify()}
-                className="bg-white/25 p-5 rounded-2xl w-full"
-              >
-                <TextInput
-                  className="text-white font-bold"
-                  placeholder="Email"
-                  placeholderTextColor="white"
-                  onChangeText={handleChange("email")}
-                  onBlur={handleBlur("email")}
-                  value={values.email}
-                />
-                {touched.email && errors.email && (
-                  <Text className="text-red-500">{errors.email}</Text>
-                )}
-              </Animated.View>
-              <Animated.View
-                entering={FadeInDown.delay(400).springify()}
-                className="bg-white/25 p-5 rounded-2xl w-full"
-              >
-                <TextInput
-                  className="text-white font-bold"
-                  placeholder="Contraseña"
-                  placeholderTextColor="white"
-                  secureTextEntry
-                  onChangeText={handleChange("password")}
-                  onBlur={handleBlur("password")}
-                  value={values.password}
-                />
-                {touched.password && errors.password && (
-                  <Text className="text-red-500">{errors.password}</Text>
-                )}
-              </Animated.View>
-
-              <Animated.View
-                entering={FadeInDown.delay(600).springify()}
-                className="flex-row"
-              >
-                <Text className="text-white">¿Olvidaste tu contraseña?</Text>
-                <TouchableOpacity onPress={() => navigation.push("Signup")}>
-                  <Text className="font-bold text-yellow-300">
-                    {" "}
-                    Recuperar contraseña
-                  </Text>
-                </TouchableOpacity>
-              </Animated.View>
-
-              <Animated.View
-                entering={FadeInDown.delay(800).springify()}
-                className="w-full"
-              >
-                <TouchableOpacity
-                  className="w-full p-3 bg-yellow-500 rounded-2xl mb-2"
-                  onPress={formikSubmit}
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit: formikSubmit,
+              values,
+              errors,
+              touched,
+            }) => (
+              <View className="flex items-center mx-4 space-y-4">
+                <Animated.View
+                  entering={FadeInDown.delay(200).springify()}
+                  className="bg-white/25 p-4 rounded-2xl w-full"
                 >
-                  <Text className="text-center font-bold text-white">
-                    Iniciar Sesión
+                  <TextInput
+                    className="text-white font-bold text-lg"
+                    placeholder="Correo electrónico"
+                    placeholderTextColor="white"
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    value={values.email}
+                  />
+                </Animated.View>
+                {touched.email && errors.email && (
+                  <Text className="w-full text-lg text-red-500">
+                    {errors.email}
                   </Text>
-                </TouchableOpacity>
-              </Animated.View>
-            </View>
-          )}
-        </Formik>
+                )}
+                <Animated.View
+                  entering={FadeInDown.delay(400).springify()}
+                  className="bg-white/25 p-4 rounded-2xl w-full"
+                >
+                  <TextInput
+                    className="text-white font-bold text-lg"
+                    placeholder="Contraseña"
+                    placeholderTextColor="white"
+                    secureTextEntry
+                    onChangeText={handleChange("password")}
+                    onBlur={handleBlur("password")}
+                    value={values.password}
+                  />
+                </Animated.View>
+                {touched.password && errors.password && (
+                  <Text className="w-full text-lg text-red-500">
+                    {errors.password}
+                  </Text>
+                )}
+                <Animated.View
+                  entering={FadeInDown.delay(800).springify()}
+                  className="w-full"
+                >
+                  <TouchableOpacity
+                    className="w-full p-3 bg-primary rounded-2xl mb-2"
+                    onPress={formikSubmit}
+                  >
+                    <Text className="text-center font-bold text-white">
+                      Iniciar Sesión
+                    </Text>
+                  </TouchableOpacity>
+                </Animated.View>
+                <Animated.View
+                  entering={FadeInDown.delay(600).springify()}
+                  className="flex-row"
+                >
+                  <Text className="text-white">¿Olvidaste tu contraseña?</Text>
+                  <TouchableOpacity onPress={() => navigation.push("Signup")}>
+                    <Text className="font-bold text-primary">
+                      {" "}
+                      Recuperar contraseña
+                    </Text>
+                  </TouchableOpacity>
+                </Animated.View>
+              </View>
+            )}
+          </Formik>
+        </View>
       </View>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e36d12",
+        secondary: "#c4e3af",
+        accent: "#146e4a",
+        neutral: "#78716c",
+        "base-100": "#f5f3ed",
+        "base-200": "#f7f5f0",
+        "base-300": "#faf9f5",
+        info: "#f29f18",
+        success: "#a3e635",
+        warning: "#fde047",
+        error: "#dc2626",
+      },
+    },
   },
   plugins: [],
 };
